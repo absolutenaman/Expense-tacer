5414f30a796ed2673ef61923f0d145043e306d51
absolutenaman <absolute.naman@gmail.com>
2022-01-18 20:03:37 +0530

    Second commit

--- src/components/ExpensesItem.js
+++ src/components/ExpensesItem.js
@@ -1,20 +1,27 @@
 import '../components/ExpenseItem.css'
 
-function ExpenseItems()
+function ExpenseItems(props)
 {
-    const date=new Date(27,2,1999);
-    // console.log(date.toISOString());
-    const cost="$247.57";
-    const expense_title="Car Insurance";
+
+    let date=props.date.getDate();
+    let Month =props.date.toLocaleString('default', { month: 'long' });
+    let year=props.date.getYear();
+    // console.log(props.date)
+    
     return (
 
-
         <div className='expense-item'>
-    <div>{date.toISOString()}</div>
+
+    <div>
+        <div>{Month}</div>
+        <div>{props.date.getFullYear()}</div>
+        <div>{date}</div>
+        
+        </div>
 
     <div className='expense-item__description'>
-    <h2>{expense_title}</h2>
-    <div className='expense-item__price'>{cost}</div>
+    <h2>{props.title}</h2>
+    <div className='expense-item__price'>{props.amount}</div>
     </div>
     </div>
     );
